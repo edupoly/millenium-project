96ce962b697e84644bcec05e998742624e0c4399
edupoly <info.edupoly@gmail.com>
2023-02-24 18:36:18 +0530

    totalbill

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,25 +1,51 @@
 import { connect } from "react-redux";
+import React,{useEffect,useState} from 'react'
 import { serviceTypeCost } from "../store/pricelist";
 function Billing(props) {
-  console.log(props)
-  var booking = props.booking.booking;
-                    
-  var bill = [];
-  var selectedServices=Object.keys(booking.serviceList);
-  selectedServices.forEach((s)=>{
-    var billServiceItem={
-      [s]:[]
-    }
-    console.log(booking.serviceList[s])
-    booking.serviceList[s].forEach((st)=>{
-      billServiceItem[s].push({[st]:serviceTypeCost[s][st][booking.cartype]})
+  const [bill, setBill] = useState([])
+  const [billTotal, setBillTotal] = useState(0)
+  useEffect(()=>{
+    var booking = props.booking.booking;
+    var selectedServices=Object.keys(booking.serviceList);
+    var temp = [];
+    var sum =0;
+    
+    selectedServices.forEach((s)=>{
+      var billServiceItem={
+        [s]:[]
+      }
+      booking.serviceList[s].forEach((st)=>{
+        sum = sum+serviceTypeCost[s][st][booking.cartype];
+        billServiceItem[s].push({[st]:serviceTypeCost[s][st][booking.cartype]})
+      })
+      temp.push(billServiceItem)
     })
-    bill.push(billServiceItem)
-  })
-  console.log(bill)
+    console.log("temp",temp)
+    console.log("sum",sum)
+    setBill([...temp])
+    setBillTotal(sum)
+  },[props.booking.booking])
+  function total(c){
+    setBillTotal(billTotal+c)
+  }
   return (
     <div>
       <h1>Billing</h1>
+      <ul>
+        {
+          bill&& bill.map((st,i)=>{
+            return <li key={i}>
+                {Object.keys(st)[0]}
+                <ul>
+                  {st[Object.keys(st)[0]].map((s,i)=>{
+                    return <li>{Object.keys(s)[0]}:{s[Object.keys(s)[0]]}</li>
+                  })}
+                </ul>
+              </li>
+          })
+        }
+      </ul>
+      <h1>TotalBill:{billTotal}</h1>
     </div>
   )
 }
